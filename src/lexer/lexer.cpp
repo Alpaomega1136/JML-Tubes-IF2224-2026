dd9f9db111c3a365f1c0201f28c6f440c2d00033
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-03-31 13:23:11 +0700

    feat: stringcon and charcon handler

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -14,23 +14,17 @@ vector<Token> tokenize(const std::string& filename) {
     State curr_state = START_STATE;
     std::string curr_value = "";
     vector<Token> tokens;
-    auto pushtoken = [&](ListToken type, string val){
-        tokens.push_back(Token(type, val));
+    auto pushtoken = [&](ListToken type){
+        tokens.push_back(Token(type, curr_value));
         curr_state = START_STATE;
         curr_value ="";
     };
     while(file.get(curr_char)) {
-
-        switch(curr_state){
-            case START_STATE:
-                switch(curr_char) {
-                    case '\'':
-                        curr_state = START_QUOTE_STATE;
-                }
+        switch(curr_state) {
             case START_QUOTE_STATE:
                 switch(curr_char) {
                     case '\'':
-                        curr_state = STRING_END_STATE;
+                        curr_state = EMPTY_STRING_STATE;
                     default :
                         curr_state = CHAR_STATE;
                         curr_value += curr_char;
@@ -43,10 +37,56 @@ vector<Token> tokenize(const std::string& filename) {
                         curr_state = STRING_ONGOING_STATE;
                         curr_value += curr_char;
                 }
+            case CHAR_END_STATE :
+                switch(curr_char) {
+                    case '\'':
+                        curr_state = STRING_ONGOING_STATE;
+                        curr_value += curr_char;
+                    default:
+                        pushtoken(charcon);
+                }
+            case STRING_ONGOING_STATE :
+                switch(curr_char) {
+                    case '\'':
+                        curr_state = STRING_END_STATE;
+                    default:
+                        curr_value += curr_char;
+                }
+            case EMPTY_STRING_STATE :
+                switch(curr_char) {
+                    case '\'':
+                        curr_state = CHAR_STATE;
+                        curr_value += curr_char;
+                    default :
+                        pushtoken(stringcon);
+                }
+            case STRING_END_STATE :
+                switch(curr_char) {
+                    case '\'':
+                        curr_state = STRING_ONGOING_STATE;
+                        curr_value += curr_char;
+                    default:
+                        pushtoken(stringcon);
+                }
+        }
+
+        if (curr_state == START_STATE) {
+            switch(curr_char) {
+                case '\'':
+                    curr_state = START_QUOTE_STATE;
+            }   
         }
     }
+    return tokens;
 }
 
-string tokenTypeToString(ListToken list, const std::string& value);
+string tokenTypeToString(const Token& t) {
+    switch(t.type) {
+        case stringcon:
+            return "stringcon ('" + t.value +"')";
+        case charcon:
+            return "charcon ('" + t.value +"')";
+    }
+};
 
 ListToken checkKeyword(const std::string& word);
