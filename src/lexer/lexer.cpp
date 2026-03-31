6dadf2827b61331807ea1bd600f1d008a9dbc6d8
Mahatma Brahmana <13524015@mahasiswa.itb.ac.id>
2026-03-31 17:45:46 +0700

    feat : add tokenize for operator

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -20,12 +20,95 @@ vector<Token> tokenize(const std::string& filename) {
         curr_value ="";
     };
     while(file.get(curr_char)) {
+        switch (curr_state){
+            case PLUS_STATE: 
+                pushtoken(ListToken::plus, "+");      
+                break;
+            case MINUS_STATE: 
+                pushtoken(ListToken::minus, "-");     
+                break;
+            case TIMES_STATE: 
+                pushtoken(times, "*");     
+                break;
+            case RDIV_STATE: 
+                pushtoken(rdiv, "/");      
+                break;
+            case EQL_STATE: 
+                if (curr_char == '=') {
+                    pushtoken(eql, "=="); 
+                    continue; 
+                }
+                pushtoken(eql, "=");
+                break;
+            case COMMA_STATE: 
+                pushtoken(comma, ",");     
+                break;
+            case SEMICOLON_STATE: 
+                pushtoken(semicolon, ";"); 
+                break;
+            case COLON_STATE: 
+                if (curr_char == '=') {
+                    pushtoken(becomes, ":="); 
+                    continue;
+                }
+                pushtoken(colon, ":"); 
+                break;
+            case PERIOD_STATE: 
+                pushtoken(period, ".");
+            case LPARENT_STATE: 
+                if (curr_char == '*') {  
+                    curr_state = COMMENT2_STATE;
+                    continue; 
+                }
+                pushtoken(lparent, "("); 
+                break;
+            case RPARENT_STATE: 
+                pushtoken(lbrack, "[");
+                break;
+            case LBRACK_STATE: 
+                pushtoken(lbrack, "[");    
+                break;
+            case RBRACK_STATE: 
+                pushtoken(rbrack, "]");    
+                break;
+            case LSS_STATE: 
+                if (curr_char == '=') { 
+                    pushtoken(leq, "<="); 
+                    continue; 
+                }
+                if (curr_char == '>') { 
+                    pushtoken(neq, "<>"); 
+                    continue; 
+                }
+                pushtoken(lss, "<"); 
+                break;
+            case GTR_STATE: 
+                if (curr_char == '=') { 
+                    pushtoken(geq, ">="); 
+                    continue; 
+                }
+                pushtoken(gtr, ">"); 
+                break;
+        }
 
         switch(curr_state){
             case START_STATE:
-                switch(curr_char) {
-                    case '\'':
-                        curr_state = START_QUOTE_STATE;
+                switch (curr_char) {
+                    case '+': curr_state = PLUS_STATE;      break;
+                    case '-': curr_state = MINUS_STATE;     break;
+                    case '*': curr_state = TIMES_STATE;     break;
+                    case '/': curr_state = RDIV_STATE;      break;
+                    case '=': curr_state = EQL_STATE;       break;
+                    case ',': curr_state = COMMA_STATE;     break;
+                    case ';': curr_state = SEMICOLON_STATE; break;
+                    case ':': curr_state = COLON_STATE;     break;
+                    case '.': curr_state = PERIOD_STATE;    break;
+                    case '(': curr_state = LPARENT_STATE;   break;
+                    case ')': curr_state = RPARENT_STATE;   break;
+                    case '[': curr_state = LBRACK_STATE;    break;
+                    case ']': curr_state = RBRACK_STATE;    break;
+                    case '<': curr_state = LSS_STATE;       break;
+                    case '>': curr_state = GTR_STATE;       break;
                 }
             case START_QUOTE_STATE:
                 switch(curr_char) {
