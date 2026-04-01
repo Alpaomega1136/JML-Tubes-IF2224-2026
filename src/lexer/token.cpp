ab7e68ffdee5ae1c0033d819514f74266105eea7
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-04-01 08:19:07 +0700

    feat: added the missing token values to  tokenTypeToString

--- src/lexer/token.cpp
+++ src/lexer/token.cpp
@@ -10,14 +10,24 @@ const std::string Token::tokenTypeToString() const {
             return "stringcon (" + value + ")";
         case charcon:
             return "charcon (" + value + ")";
+        case notsy:
+            return "notsy";
         case ListToken::plus:
             return "plus";
         case ListToken::minus:
             return "minus";
         case times:
             return "times";
+        case idiv:
+            return "div";
         case rdiv:
             return "rdiv";
+        case imod:
+            return "imod";
+        case andsy:
+            return "andsy";
+        case orsy:
+            return "orsy";
         case eql:
             return "eql";
         case neq:
@@ -48,6 +58,52 @@ const std::string Token::tokenTypeToString() const {
             return "colon";
         case becomes:
             return "becomes";
+        case constsy:
+            return "constsy";
+        case typesy:
+            return "typesy";
+        case varsy:
+            return "varsy";
+        case functionsy:
+            return "functionsy";
+        case proceduresy:
+            return "proceduresy";
+        case arraysy:
+            return "arraysy";
+        case recordsy:
+            return "recordsy";
+        case programsy:
+            return "programsy";
+        case ident:
+            return "ident (" + value + ")";
+        case beginsy:
+            return "beginsy";
+        case ifsy:
+            return "ifsy";
+        case casesy:
+            return "casesy";
+        case repeatsy:
+            return "repeatsy";
+        case whilesy:
+            return "whilesy";
+        case forsy:
+            return "forsy";
+        case endsy:
+            return "endsy";
+        case elsesy:
+            return "elsesy";
+        case untilsy:
+            return "untilsy";
+        case ofsy:
+            return "ofsy";
+        case dosy:
+            return "dosy";
+        case tosy:
+            return "tosy";
+        case downtosy:
+            return "downtosy";
+        case thensy:
+            return "thensy";
         case comment:
             return "comment (" + value + ")";
         case unknown:
