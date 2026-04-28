85b34a150ccbc48b774f2487346f953db62b3999
RND815 <reynard15082006@gmail.com>
2026-04-28 18:02:38 +0700

    feat: add implement some parser methods

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -459,3 +459,358 @@ TreeParser* Parser::ParameterGroup() {
         throw std::runtime_error(std::string("Parsing error in ParameterGroup: ") + e.what());
     }
 }
+
+// <compound-statement> -> beginsy statement-list endsy
+TreeParser* Parser::CompoundStatement() {
+    TreeParser* node = new TreeParser("<compound-statement>");
+    try {
+        node->addChild(match(beginsy));
+        node->addChild(StatementList());
+        node->addChild(match(endsy));
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <compound-statement>: ") + e.what());
+    }
+}
+
+// <statement-list> -> statement (semicolon statement)*
+TreeParser* Parser::StatementList() {
+    TreeParser* node = new TreeParser("<statement-list>");
+    try {
+        node->addChild(Statement());
+        while (check(semicolon)) {
+            node->addChild(match(semicolon));
+            node->addChild(Statement());
+        }
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <statement-list>: ") + e.what());
+    }
+}
+ 
+// <statement> -> assignment-statement | if-statement | case-statement
+//              | while-statement | repeat-statement | for-statement
+//              | procedure/function-call | compound-statement | (empty)
+//
+// Ambiguitas ident:
+//   - Jika ident diikuti ':=' -> assignment
+//   - Jika ident diikuti '(' atau diikuti statement-terminator -> proc/func call
+TreeParser* Parser::Statement() {
+    TreeParser* node = new TreeParser("<statement>");
+    try {
+        if (check(ident)) {
+            if (checkNext(becomes)) {
+                node->addChild(AssignmentStatement());
+            } else {
+                node->addChild(ProcedureFunctionCall());
+            }
+        } else if (check(ifsy)) {
+            node->addChild(IfStatement());
+        } else if (check(casesy)) {
+            node->addChild(CaseStatement());
+        } else if (check(whilesy)) {
+            node->addChild(WhileStatement());
+        } else if (check(repeatsy)) {
+            node->addChild(RepeatStatement());
+        } else if (check(forsy)) {
+            node->addChild(ForStatement());
+        } else if (check(beginsy)) {
+            node->addChild(CompoundStatement());
+        }
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <statement>: ") + e.what());
+    }
+}
+ 
+// <assignment-statement> -> ident becomes expression
+TreeParser* Parser::AssignmentStatement() {
+    TreeParser* node = new TreeParser("<assignment-statement>");
+    try {
+        node->addChild(match(ident));
+        node->addChild(match(becomes));
+        node->addChild(Expression());
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <assignment-statement>: ") + e.what());
+    }
+}
+ 
+// <if-statement> -> ifsy expression thensy statement (elsesy statement)?
+TreeParser* Parser::IfStatement() {
+    TreeParser* node = new TreeParser("<if-statement>");
+    try {
+        node->addChild(match(ifsy));
+        node->addChild(Expression());
+        node->addChild(match(thensy));
+        node->addChild(Statement());
+        if (check(elsesy)) {
+            node->addChild(match(elsesy));
+            node->addChild(Statement());
+        }
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <if-statement>: ") + e.what());
+    }
+}
+ 
+// <case-statement> -> casesy expression ofsy case-block endsy
+TreeParser* Parser::CaseStatement() {
+    TreeParser* node = new TreeParser("<case-statement>");
+    try {
+        node->addChild(match(casesy));
+        node->addChild(Expression());
+        node->addChild(match(ofsy));
+        node->addChild(CaseBlock());
+        node->addChild(match(endsy));
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <case-statement>: ") + e.what());
+    }
+}
+ 
+// <case-block> -> constant (comma constant)* colon statement 
+//                (semicolon case-block?)*
+TreeParser* Parser::CaseBlock() {
+    TreeParser* node = new TreeParser("<case-block>");
+    try {
+        node->addChild(Constant());
+        while (check(comma)) {
+            node->addChild(match(comma));
+            node->addChild(Constant());
+        }
+        node->addChild(match(colon));
+        node->addChild(Statement());
+ 
+        while (check(semicolon)) {
+            int savedPos = pos;
+            node->addChild(match(semicolon));
+            bool hasNextCase = check(ident) || check(intcon) || check(realcon) ||
+                               check(charcon) || check(stringcon) ||
+                               check(plus) || check(minus);
+            if (hasNextCase && !check(endsy)) {
+                node->addChild(CaseBlock());
+            }
+            if (!hasNextCase) break;
+        }
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <case-block>: ") + e.what());
+    }
+}
+ 
+// <while-statement> -> whilesy expression dosy statement
+TreeParser* Parser::WhileStatement() {
+    TreeParser* node = new TreeParser("<while-statement>");
+    try {
+        node->addChild(match(whilesy));
+        node->addChild(Expression());
+        node->addChild(match(dosy));
+        node->addChild(Statement());
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <while-statement>: ") + e.what());
+    }
+}
+ 
+// <repeat-statement> -> repeatsy statement-list untilsy expression
+TreeParser* Parser::RepeatStatement() {
+    TreeParser* node = new TreeParser("<repeat-statement>");
+    try {
+        node->addChild(match(repeatsy));
+        node->addChild(StatementList());
+        node->addChild(match(untilsy));
+        node->addChild(Expression());
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <repeat-statement>: ") + e.what());
+    }
+}
+ 
+// <for-statement> -> forsy ident becomes expression (tosy|downtosy)
+//                    expression dosy statement
+TreeParser* Parser::ForStatement() {
+    TreeParser* node = new TreeParser("<for-statement>");
+    try {
+        node->addChild(match(forsy));
+        node->addChild(match(ident));
+        node->addChild(match(becomes));
+        node->addChild(Expression());
+        if (check(tosy))           node->addChild(match(tosy));
+        else if (check(downtosy))  node->addChild(match(downtosy));
+        else throw std::runtime_error("Expected 'to' or 'downto' in <for-statement>");
+        node->addChild(Expression());
+        node->addChild(match(dosy));
+        node->addChild(Statement());
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <for-statement>: ") + e.what());
+    }
+}
+ 
+// <procedure/function-call> -> ident (lparent parameter-list rparent)?
+TreeParser* Parser::ProcedureFunctionCall() {
+    TreeParser* node = new TreeParser("<procedure/function-call>");
+    try {
+        node->addChild(match(ident));
+        if (check(lparent)) {
+            node->addChild(match(lparent));
+            if (!check(rparent)) {
+                node->addChild(ParameterList());
+            }
+            node->addChild(match(rparent));
+        }
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <procedure/function-call>: ") + e.what());
+    }
+}
+ 
+// <parameter-list> -> expression (comma expression)*
+TreeParser* Parser::ParameterList() {
+    TreeParser* node = new TreeParser("<parameter-list>");
+    try {
+        node->addChild(Expression());
+        while (check(comma)) {
+            node->addChild(match(comma));
+            node->addChild(Expression());
+        }
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <parameter-list>: ") + e.what());
+    }
+}
+ 
+// <expression> -> simple-expression (relational-operator simple-expression)?
+TreeParser* Parser::Expression() {
+    TreeParser* node = new TreeParser("<expression>");
+    try {
+        node->addChild(SimpleExpression());
+        if (check(eql) || check(neq) || check(gtr) ||
+            check(geq) || check(lss) || check(leq)) {
+            node->addChild(RelationalOperator());
+            node->addChild(SimpleExpression());
+        }
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <expression>: ") + e.what());
+    }
+}
+ 
+// <simple-expression> -> (plus|minus)? term (additive-operator term)*
+TreeParser* Parser::SimpleExpression() {
+    TreeParser* node = new TreeParser("<simple-expression>");
+    try {
+        if (check(plus))       node->addChild(match(plus));
+        else if (check(minus)) node->addChild(match(minus));
+ 
+        node->addChild(Term());
+        while (check(plus) || check(minus) || check(orsy)) {
+            node->addChild(AdditiveOperator());
+            node->addChild(Term());
+        }
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <simple-expression>: ") + e.what());
+    }
+}
+ 
+// <term> -> factor (multiplicative-operator factor)*
+TreeParser* Parser::Term() {
+    TreeParser* node = new TreeParser("<term>");
+    try {
+        node->addChild(Factor());
+        while (check(times) || check(rdiv) || check(idiv) ||
+               check(imod) || check(andsy)) {
+            node->addChild(MultiplicativeOperator());
+            node->addChild(Factor());
+        }
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <term>: ") + e.what());
+    }
+}
+ 
+// <factor> -> ident | intcon | realcon | charcon | stringcon
+//           | lparent expression rparent
+//           | notsy factor
+//           | procedure/function-call   (ident '(' ... ')')
+TreeParser* Parser::Factor() {
+    TreeParser* node = new TreeParser("<factor>");
+    try {
+        if (check(ident)) {
+            if (checkNext(lparent)) node->addChild(ProcedureFunctionCall());
+            else                    node->addChild(match(ident));
+        } else if (check(intcon)) {
+            node->addChild(match(intcon));
+        } else if (check(realcon)) {
+            node->addChild(match(realcon));
+        } else if (check(charcon)) {
+            node->addChild(match(charcon));
+        } else if (check(stringcon)) {
+            node->addChild(match(stringcon));
+        } else if (check(lparent)) {
+            node->addChild(match(lparent));
+            node->addChild(Expression());
+            node->addChild(match(rparent));
+        } else if (check(notsy)) {
+            node->addChild(match(notsy));
+            node->addChild(Factor());
+        } else {
+            throw std::runtime_error(
+                "Syntax error: unexpected token '" +
+                currentToken().tokenTypeToString() +
+                "' in <factor>");
+        }
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <factor>: ") + e.what());
+    }
+}
+ 
+// <relational-operator> -> eql | neq | gtr | geq | lss | leq
+TreeParser* Parser::RelationalOperator() {
+    TreeParser* node = new TreeParser("<relational-operator>");
+    try {
+        if      (check(eql)) node->addChild(match(eql));
+        else if (check(neq)) node->addChild(match(neq));
+        else if (check(gtr)) node->addChild(match(gtr));
+        else if (check(geq)) node->addChild(match(geq));
+        else if (check(lss)) node->addChild(match(lss));
+        else if (check(leq)) node->addChild(match(leq));
+        else throw std::runtime_error("Expected relational operator");
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <relational-operator>: ") + e.what());
+    }
+}
+ 
+// <additive-operator> -> plus | minus | orsy
+TreeParser* Parser::AdditiveOperator() {
+    TreeParser* node = new TreeParser("<additive-operator>");
+    try {
+        if      (check(plus))  node->addChild(match(plus));
+        else if (check(minus)) node->addChild(match(minus));
+        else if (check(orsy))  node->addChild(match(orsy));
+        else throw std::runtime_error("Expected additive operator (+, -, or)");
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <additive-operator>: ") + e.what());
+    }
+}
+ 
+// <multiplicative-operator> -> times | rdiv | idiv | imod | andsy
+TreeParser* Parser::MultiplicativeOperator() {
+    TreeParser* node = new TreeParser("<multiplicative-operator>");
+    try {
+        if      (check(times)) node->addChild(match(times));
+        else if (check(rdiv))  node->addChild(match(rdiv));
+        else if (check(idiv))  node->addChild(match(idiv));
+        else if (check(imod))  node->addChild(match(imod));
+        else if (check(andsy)) node->addChild(match(andsy));
+        else throw std::runtime_error("Expected multiplicative operator (*, /, div, mod, and)");
+        return node;
+    } catch (const std::runtime_error& e) {
+        throw std::runtime_error(std::string("Parsing error in <multiplicative-operator>: ") + e.what());
+    }
+}
